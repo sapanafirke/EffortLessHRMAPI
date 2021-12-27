3bae4c9b5f34a2b1444abba5140b9c708e145b62
arsteg <83569152+arsteg@users.noreply.github.com>
2021-12-27 21:36:37 +0530

    removed console log statements

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ var bodyParser = require('body-parser');
 var itemRouter = require('./routes/item');
 var tourRouter = require('./routes/tourRoutes');
 var userRouter = require('./routes/userRoutes');
-
+const compression = require('compression');
 var authRouter = require('./routes/authRouter');
 const AppError = require('./utils/appError');
 
 var app = express();
 
 app.use(express.json({ lmit: '10kb' }));
+
+app.use(compression);
 
 // Each request will contain requested time
 app.use((req, res, next) => {    
