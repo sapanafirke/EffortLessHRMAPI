6c824a38c49babe23ffa69b4715ea41a254f089b
arsteg <83569152+arsteg@users.noreply.github.com>
2022-05-06 10:49:19 +0530

    Added cors

--- app.js
+++ app.js
@@ -7,15 +7,23 @@ var userRouter = require('./routes/userRoutes');
 var timeLogRouter = require('./routes/timeLogRoutes');
 var holidayCalendarRouter = require('./routes/holidayCalendarRouter');
 const compression = require('compression');
+const cors =  require('cors');
 var authRouter = require('./routes/authRouter');
 const AppError = require('./utils/appError');
 var app = express();
 
 app.use(express.json({ lmit: '10kb' }));
 //app.use(compression);
+app.use(cors());
+
+app.options('*',cors());
 
 // Each request will contain requested time
 app.use((req, res, next) => {    
+  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    
+
   console.log('received the request');  
   next(); // run next middleware in stack
   });  
