ddf4096333256b600150dcf8d83e64ef794ea4ed
sapana.firke@arsteg.com <sapana.firke@arsteg.com>
2022-05-17 17:37:56 +0530

    committed app.js

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var tourRouter = require('./routes/tourRoutes');
 var userRouter = require('./routes/userRoutes');
 var timeLogRouter = require('./routes/timeLogRoutes');
 var commonRouter = require('./routes/commonRouter');
+var companyRouter = require('./routes/companyRouter');
+var projectRouter = require('./routes/projectRouter');
 var holidayCalendarRouter = require('./routes/holidayCalendarRouter');
+var taskRouter = require('./routes/taskRouter');
 const compression = require('compression');
 const cors =  require('cors');
 var authRouter = require('./routes/authRouter');
@@ -34,6 +37,9 @@ app.use((req, res, next) => {
   app.use('/api/v1/tours', tourRouter);
   app.use('/api/v1/timeLogs', timeLogRouter);
   app.use('/api/v1/holidayCalendar', holidayCalendarRouter);
+  app.use('/api/v1/company', companyRouter);
+  app.use('/api/v1/project', projectRouter);
+  app.use('/api/v1/task', taskRouter);
   // api route for common API like Country , Role , Permission , RolePermission
   app.use('/api/v1/common', commonRouter);
 module.exports = app;
