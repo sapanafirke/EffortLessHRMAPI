6feb41c12ab47c65f170fc0a4b888be1fc2bb627
Mohamad Rafi <dotnetexpertdev@gmail.com>
2023-03-31 14:37:20 +0530

    header value changes

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.set("email", path.join(__dirname, "email"));
 // Each request will contain requested time
 app.use((req, res, next) => {
 
-  res.header("Access-Control-Allow-Origin","http://localhost:4200");
+  res.header("Access-Control-Allow-Origin","*");
   
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
