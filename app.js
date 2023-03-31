d46d45c4b1aca731587c1abba43958eb8f2ec478
Mohamad Rafi <dotnetexpertdev@gmail.com>
2023-03-31 14:40:03 +0530

    header changes

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.set("email", path.join(__dirname, "email"));
 // Each request will contain requested time
 app.use((req, res, next) => {
 
-  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Origin","https://effort-less-hrm-web.vercel.app");
   
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
